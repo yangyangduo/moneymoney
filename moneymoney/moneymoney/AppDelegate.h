7b9193f4453660e5d7cc189a6bed0da62a4c42b6
yangyangduo <zhaoyang@vip.163.com>
2014-04-22 17:05:02 +0800

    init project

---
+++ moneymoney/moneymoney/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  moneymoney
+//
+//  Created by Zhao yang on 4/22/14.
+//  Copyright (c) 2014 hentre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
